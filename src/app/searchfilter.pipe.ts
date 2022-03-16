540287f0e2bbbc773667e9e6c862cef0188d6a90
jishnu_TR <jishnu@newagesmb.com>
2022-03-16 18:47:46 +0530

    pagination,forgot password,otp verification,update password changed from modal to 3 different components

---
+++ src/app/searchfilter.pipe.ts
@@ -0,0 +1,15 @@
+// import { Pipe, PipeTransform } from '@angular/core';
+// import { UserDetailspage } from './model/models/Models.model';
+
+// @Pipe({
+//   name: 'searchfilter'
+// })
+// export class SearchfilterPipe implements PipeTransform {
+
+//   transform(userData : UserDetailspage[],searchValue :string): UserDetailspage {
+//     if(!userData || searchValue)
+//     {
+//       return UserDetailspage;
+//     }
+
+// }
